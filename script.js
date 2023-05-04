0c7addb3bb52bccc5297c9e6c4ea2ce8ca4e151b
painter98 <120320713+painter98@users.noreply.github.com>
2023-05-04 19:39:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+let radius = prompt('enter Radius of the circle');
+
+function Area(radius){
+	let area = Math.round(Math.PI*radius*radius,2);
+	return `The area of the circle with radius 4 is ${area}`;
+}
+console.log(Area(radius));
